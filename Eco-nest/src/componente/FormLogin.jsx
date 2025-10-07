24e8c0bb3dd843c73d0cf1082354acf5cf713da9
sinara_santinoni <sinara_santinoni@hotmail.com>
2025-10-07 17:21:14 -0300

    inicio da tela login

--- Eco-nest/src/componente/FormLogin.jsx
+++ Eco-nest/src/componente/FormLogin.jsx
@@ -1,5 +1,35 @@
-export function FormLogin(){
-    return <>
-    <h1>login</h1>
-    </>
+export function FormLogin() {
+  
+   const verdeTema = '#2F5E36';
+
+       const estiloBorda = {
+        border: `1px solid ${verdeTema}`,
+        borderRadius: '5px',
+        overflow: 'hidden',
+    };
+
+  return <>
+
+    
+   <div class="d-flex justify-content-center align-items-center min-vh-100 ">
+    <div class="col-md-4">
+        
+        <h3 className="text-center mb-4" style={{color:verdeTema}}> Faça o seu Login </h3>
+        
+        <form>
+            <div class="input-group mb-3 input-login-custom">
+                <span class="input-group-text"><i class="bi bi-envelope-at"></i></span>
+                <input type="email" class="form-control" placeholder="Seu e-mail" required />
+            </div>
+
+            <div class="input-group mb-3 input-login-custom">
+                <span class="input-group-text"><i class="bi bi-lock"></i></span>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" required />
+            </div>
+
+            <button type="submit" class="btn btn-lg btn-success w-100">ENTRAR</button>
+        </form>
+    </div>
+</div>
+  </>
 }

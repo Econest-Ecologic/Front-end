5e6426b24a4eef6c7ab21872abf7845696dd765b
MarcosPaulo-web <marcospaulobitt@gmail.com>
2025-10-08 17:31:39 -0300

    feat:estilização do forms

--- Eco-nest/src/componente/Navbar.jsx
+++ Eco-nest/src/componente/Navbar.jsx
@@ -1,27 +1,36 @@
 import { useState } from "react"
 
 export function Navbar() {
-
   const [tema, setTema] = useState("dark");
+  const [numeroCarrinho,setNumeroCarrinho] = useState(0);
+  function toggleTheme() {
+    const newTheme = tema === "light" ? "dark" : "light";
+    setTema(newTheme);
+    document.documentElement.setAttribute("data-bs-theme", newTheme);
+  }
 
-    function toggleTheme() {
-        const newTheme = tema === "light" ? "dark" : "light";
-        setTema(newTheme);
-        document.documentElement.setAttribute("data-bs-theme", newTheme);
-    }
-
-
-    return <>
- <nav className="navbar navbar-expand-lg bg-success-subtle">
+  return (
+    <nav className="navbar navbar-expand-lg bg-success-subtle">
   <div className="container-fluid">
 
     <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+      aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
 
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-center gap-2">
+        
+        <li className="nav-item me-3">
+          <form className="d-flex input-group" role="search">
+            <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success input-group-text" type="submit">Search</button>
+          </form>
+        </li>
+
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
@@ -31,18 +40,26 @@ export function Navbar() {
         <li className="nav-item">
           <a className="nav-link" href="#">Contato</a>
         </li>
-        <li classNameName="na-item">
-            <button className={tema == 'dark' ? "btn btn-outline-warning border border-0":"btn btn-outline-dark border border-0"} onClick={toggleTheme}>{tema == 'dark' ? <i className="bi bi-brightness-high-fill"></i> : <i class="bi bi-moon"></i>}</button>
+        <li className="nav-item">
+          <button className={tema === 'dark' ? "btn btn-outline-warning border-0" : "btn btn-outline-dark border-0"} 
+            onClick={toggleTheme}>
+            {tema === 'dark' ? <i className="bi bi-brightness-high-fill"></i> : <i className="bi bi-moon"></i>}
+          </button>
         </li>
+        <li className="nav-item">
+          <button className="btn btn-outline-eco bg-transparent border-0 position-relative"><i class="bi bi-cart"></i>
+          <span className={tema == 'dark' ? "badge  text-light position-absolute top-0 start-50": "badge  text-dark position-absolute top-0 start-50"}>{numeroCarrinho}</span>
+          </button>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link btn btn btn-eco">Entrar</a>
+        </li>
+
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
+
   </div>
 </nav>
-        
-    </>
-}
 
+  )
+}

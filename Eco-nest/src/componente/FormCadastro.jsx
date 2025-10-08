e3208a4177f5a54578c8ac1265e658fb4c78c94d
MarcosPaulo-web <marcospaulobitt@gmail.com>
2025-10-08 14:29:32 -0300

    feat : componentização do input para os forms

--- Eco-nest/src/componente/FormCadastro.jsx
+++ Eco-nest/src/componente/FormCadastro.jsx
@@ -1,47 +1,29 @@
 import { BotaoTrocaLogin } from "./BotaoTrocaLogin";
+import InputForm from "./InputForm";
 
 export function FormCadastro({ trocaLogin }) {
     return <>
-    <div className="container-fluid d-flex flex-lg-row flex-column flex-wrap justify-content-center align-items-center h-50 border border-1  border-secundary p-5 ">
+        <div className="container-fluid d-flex flex-lg-row flex-column flex-wrap justify-content-center align-items-center h-50 border border-1  border-secundary p-5 ">
 
-        <div className="container-fluid d-flex justify-content-center align-items-center flex-column w-50">
-            <h1 className="text-center text-success">Realizar Cadastro</h1>
-            <img src="..\public\logoSemFundo.png" className="img-thumbnail border-0 w-50 h-50 " alt="Logo" />
+            <div className="container-fluid d-flex justify-content-center align-items-center flex-column w-50">
+                <h1 className="text-center text-success">Realizar Cadastro</h1>
+                <img src="..\public\logoSemFundo.png" className="img-thumbnail border-0 w-50 h-50 " alt="Logo" />
+            </div>
+
+            <form className="container-fluid w-50 justify-content-center">
+                <InputForm placeholder={"Nome"} icon={<i className="bi bi-person"></i>} />
+                <InputForm placeholder={"Email"} icon={<i className="bi bi-envelope-at"></i>} />
+                <InputForm placeholder={"Senha"} icon={<i className="bi bi-lock"></i>} />
+                <InputForm placeholder={"Endereço"} icon={<i className="bi bi-globe-americas"></i>} />
+                <InputForm placeholder={"Cpf"} icon={<i className="bi bi-file-earmark-lock-fill"></i>}/>
+                <InputForm placeholder={"Telefone"} icon={<i className="bi bi-telephone"></i>}/>
+        
+                <div className="d-flex gap-2 justify-content-center flex-row gap-3">
+                    <button className="btn btn-primary w-100">Cadastrar</button>
+                    <BotaoTrocaLogin trocaLogin={trocaLogin} text={"Login"} />
+                </div>
+
+            </form>
         </div>
-
-        <form className="container-fluid w-50 justify-content-center">
-            <div className="mb-3 input-group">
-                <span className="input-group-text"><i className="bi bi-person"></i></span>
-                <input type="text" placeholder="Nome" className="form-control input-success" id="nome" />
-            </div>
-            <div className="mb-3 input-group">
-                <span className="input-group-text"><i className="bi bi-envelope-at"></i>
-                </span>
-                <input type="email" placeholder="Email" className="form-control input-success" id="email" />
-            </div>
-            <div className="mb-3 input-group">
-                <span className="input-group-text "> <i className="bi bi-lock"></i></span>
-                <input type="password" placeholder="Senha" className="form-control input-success" id="senha" />
-            </div>
-            <div className="mb-3 input-group">
-                <span className="input-group-text"><i className="bi bi-globe-americas"></i>
-                </span>
-                <input type="text" placeholder="Endereço" className="form-control input-success" id="endereco" />
-            </div>
-            <div className="mb-3 input-group">
-                <span className="input-group-text"><i className="bi bi-file-earmark-lock-fill"></i></span>
-                <input type="text" placeholder="Cpf" className="form-control input-success" id="cpf" />
-            </div>
-            <div className="mb-3 input-group">
-                <span className="input-group-text"><i className="bi bi-telephone"></i>
-                </span>
-                <input type="text" placeholder="Telefone" className="form-control input-success" id="relefone" />
-            </div>
-            <div className="d-flex gap-2 justify-content-center flex-row gap-3">
-                <button className="btn btn-primary w-100">Cadastrar</button>
-                <BotaoTrocaLogin trocaLogin={trocaLogin} text={"Login"} />
-            </div>
-        </form>
-    </div>
     </>
 }
--- Eco-nest/src/componente/Navbar.jsx
+++ Eco-nest/src/componente/Navbar.jsx
@@ -12,35 +12,37 @@ export function Navbar() {
 
 
     return <>
-        <nav className="navbar navbar-expand-lg bg-success-subtle">
+ <nav className="navbar navbar-expand-lg bg-success-subtle">
+  <div className="container-fluid">
 
-            <div className="container-fluid ">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-                <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-                    <ul className="navbar-nav mb-0 me-5">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Sobre nós</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="#">Contato</a>
-                        </li>
-                    </ul>
-                    <form className="btn-group ms-2" role="search">
-                        <input className="form-control rounded-0 rounded-start-3" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit"><i className="bi bi-search"></i></button>
-                    </form>
-                    <button className="btn btn-outline-success ms-2" onClick={toggleTheme}><i className="bi bi-brightness-high-fill"></i></button>
-                </div>
-
-            </div>
-        </nav>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Sobre Nós</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contato</a>
+        </li>
+        <li classNameName="na-item">
+            <button className={tema == 'dark' ? "btn btn-outline-light border border-0":"btn btn-outline-dark border border-0"} onClick={toggleTheme}>{tema == 'dark' ? <i className="bi bi-brightness-high-fill"></i> : <i className="bi bi-moon"></i>}</button>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+        
     </>
 }
 

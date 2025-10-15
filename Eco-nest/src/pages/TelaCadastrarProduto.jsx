08285fc5455e500da5bb6dd4c00f7e3805a42a3e
sinara_santinoni <sinara_santinoni@hotmail.com>
2025-10-15 17:22:11 -0300

    feature: tela produtos

--- Eco-nest/src/pages/TelaCadastrarProduto.jsx
+++ Eco-nest/src/pages/TelaCadastrarProduto.jsx
@@ -1,7 +1,15 @@
+import { Navbar } from "../componente/Navbar"
 export function TelaCadastrarProduto(){
-     return<> 
-     
-     
-     
+     return <>
+     <Navbar></Navbar>
+     <main  className="container-fluid bg-eco border-top border-2 eco-border  d-flex justify-content-center align-items-center vh-100 ">
+
+          <div className="container bg-body rounded rounded-5">
+               <button className="btn btn-voltar border-0 fs-2 rounded-2"> <i class="bi bi-arrow-left-short"></i></button>
+          </div>
+     </main>
+
+
      </>
-}
+     
+}    

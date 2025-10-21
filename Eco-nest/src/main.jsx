a387f1a5cf94f02c6ca38d0ce8b62c8573361f3b
sinara_santinoni <sinara_santinoni@hotmail.com>
2025-10-21 16:31:24 -0300

    Feature: criação da tela editar perfil do administrador

--- Eco-nest/src/main.jsx
+++ Eco-nest/src/main.jsx
@@ -10,6 +10,7 @@ import { TelaAdm } from "./pages/TelaAdm";
 import { TelaCadastrarProduto } from "./pages/TelaCadastrarProduto";
 import { CriarAdministrador } from "./pages/CriarAdministrador";
 import PagProduto from "./componente/bodyPages/PagProduto";
+import TelaEditarPerfil from "./pages/TelaEditarPerfil";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -24,6 +25,8 @@ createRoot(document.getElementById("root")).render(
         <Route path="/cadastroProduto" element={<TelaCadastrarProduto />} />
         <Route path="/criarCadastro" element={<CriarAdministrador />} />
         <Route path="/pagProduto" element={<PagProduto />} />
+        <Route path="/editarperfil" element={<TelaEditarPerfil />} />
+        
         
       </Routes>
     </BrowserRouter>

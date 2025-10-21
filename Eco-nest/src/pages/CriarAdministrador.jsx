2b901148fed4c110e1a5cf4feee6b789b4b5ed52
sinara_santinoni <sinara_santinoni@hotmail.com>
2025-10-21 16:19:42 -0300

    Fix:finalizei a tela de criar administrador

--- Eco-nest/src/pages/CriarAdministrador.jsx
+++ Eco-nest/src/pages/CriarAdministrador.jsx
@@ -4,6 +4,8 @@ import InputFull from "../componente/InputFull"
 import InputOutline from "../componente/InputOutline"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import '../app.css';
+
 
 
 
@@ -17,22 +19,21 @@ export function CriarAdministrador() {
     const [telefone, settelefone] = useState();
 
     return <>
-        
-
+     
         <div className="d-flex justify-content-center align-items-center min-vh-100 bg-eco d-flex vh-100 ">
             <div className="p-5 custom-card-container bg-body rounded rounded-5 d-flex justify-content-center align-items-center">
              <button className="btn btn-voltar border-0 fs-2 rounded-2 align-self-start" onClick={() => { navigate(-1) }}> <i className="bi bi-arrow-left-short"></i></button>
                 
                 <div className="row">
 
-                    <div className="col-md-5 d-flex flex-column align-items-center justify-content-center text-center eco-text">
-                        <h2 className="mb-4 fw-bold">
+                    <div className="col-md-5 d-flex flex-column align-items-center justify-content-center text-center eco-text"id="logo-container">
+                        <h2 className="fw-bold" id="titulo-admin">
                             CRIAR <br /> ADMINISTRADOR
                         </h2>
                         <img
                             src="..\public\logoSemFundo.png"
                             alt="EcoNest Logo"
-                            className="img-fluid"
+                            className="img-fluid w-50"
                         />
                     </div>
 

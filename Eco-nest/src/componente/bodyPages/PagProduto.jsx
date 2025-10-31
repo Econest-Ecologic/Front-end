64646b94540396b4011d31b70efd56204966d476
Marcos <marcospaulobitt@gmail.com>
2025-10-31 20:44:28 -0300

    fix : pag de produto com titulo verde

--- Eco-nest/src/componente/bodyPages/PagProduto.jsx
+++ Eco-nest/src/componente/bodyPages/PagProduto.jsx
@@ -1,32 +1,36 @@
-import { useSearchParams } from "react-router-dom"
-import { Navbar } from "../Navbar"
+import { useSearchParams } from "react-router-dom";
+import { Navbar } from "../Navbar";
 import Card from "../Card";
-export default function PagProduto(){
-    const[searchParams] = useSearchParams();
-   const title= searchParams.get("title")
-   const listaProd = JSON.parse(decodeURIComponent(searchParams.get("listaProd")))//decotifica a var
+export default function PagProduto() {
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const listaProd = JSON.parse(
+    decodeURIComponent(searchParams.get("listaProd"))
+  ); //decotifica a var
 
-return<>
-<Navbar />
+  return (
+    <>
+      <Navbar />
 
-    <div className="container my-4">
-      <h1 className="text-center mb-4">{title}</h1>
+      <div className="container my-4">
+        <h1 className="text-center eco-text mb-4">{title}</h1>
 
-      <div className="row flex-wrap g-4">
-        {listaProd.map((p, i) => (
-          <div className="col-md-3" key={i}>
-            <Card
-              img={p.img}
-              title={p.nome}
-              desc={p.desc}
-              price={p.preco}
-              badge={p.categoria}
-              color="bg-success"
-              border="border-5 eco-border"
-            />
-          </div>
-        ))}
+        <div className="row flex-wrap g-4">
+          {listaProd.map((p, i) => (
+            <div className="col-md-3" key={i}>
+              <Card
+                img={p.img}
+                title={p.nome}
+                desc={p.desc}
+                price={p.preco}
+                badge={p.categoria}
+                color="bg-success"
+                border="border-5 eco-border"
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-</>    
+    </>
+  );
 }
